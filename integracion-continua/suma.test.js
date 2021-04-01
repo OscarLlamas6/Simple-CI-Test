6707a5d76c752ef0e9af5107bc083eb12744fc51
Oscar Llamas <oscarllamasusac@gmail.com>
2021-03-31 22:24:00 -0600

    17 commit

--- integracion-continua/suma.test.js
+++ integracion-continua/suma.test.js
@@ -4,6 +4,10 @@ test('2 + 5 = 7', () =>{
     expect(suma(5,5)).toBe(10);
 });
 
+test('2 + 2 = 4', () =>{
+    expect(suma(2,2)).toBe(4);
+});
+
 test('a + 5 = NaN', () =>{
     expect(suma('a',5)).toBe(NaN);
 });
